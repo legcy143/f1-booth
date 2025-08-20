4d9681eea5ec7eb53e6efc0ee285e7cbab3bfec2
prince <legcy143@gmail.com>
2025-08-20 13:14:46 +0530

    feat: done with basic upload and preview

--- src/app/_store/useVideoTemplateBooth.ts
+++ src/app/_store/useVideoTemplateBooth.ts
@@ -11,16 +11,22 @@ interface StoryFormState {
 export const BASE_API_URL = "https://api.quenth.com"
 
 
-
 export const useVideoTemplateBooth = create<StoryFormState>((set, get) => ({
     isUploadingVideo: false,
     video: null,
     uploadVideo: async (file: File) => {
         set({ isUploadingVideo: true });
         try {
-            let res = await uploadSingleFile(file);
-            set({ video: res });
-            return res;
+            let videoUrl = await uploadSingleFile(file);
+            const res = await axios.post(`${BASE_API_URL}/video-template-booth/generate`,
+                {
+                    "eventId": "68a5721879aa3a1e660f1634",
+                    "templateId": "68a5721879aa3a1e660f1635",
+                    "input": videoUrl
+                }
+            );
+            set({ video: res.data.data });
+            return res.data.data;
         } catch (error) {
             console.error("Error uploading video:", error);
             throw error;
